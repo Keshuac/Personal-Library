a65f72bc59f9d9fb5060cf2bb4c9b7b6dc2f786e
Keshuac <48709680+Keshuac@users.noreply.github.com>
2019-03-19 20:15:42 +1300

    Create HelloWorld Python.py

---
+++ HelloWorld Python.py
@@ -0,0 +1,18 @@
+print("Hello, World")
+
+A="hello"
+b="World"
+One = 1
+Float_One = 1.0
+Stacka,Taka="Hot",'Trash'
+print(A+" "+b)
+print(One)
+print(Float_One)
+print(Stacka+" "+Taka)
+print("apostrophes will work in double quotations only, even though you can use both singular and double quotation marks in strings")
+print("Remember the indentations")
+print("No Mixing of Strings and Numbers")
+c=1
+if c==1:
+	print("Supercalifragilisticexpialladocious")
+
